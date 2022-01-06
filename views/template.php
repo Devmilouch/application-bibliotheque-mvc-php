36a56b0ba6483f53ff38c0cf8e35832d3ac3013a
Amar Bouabbache <amarbouabbache89pro@gmail.com>
2022-01-06 01:07:06 +0100

    Meta author + title

--- views/template.php
+++ views/template.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Amar Bouabbache">
     <link rel="stylesheet" href="https://bootswatch.com/5/sketchy/bootstrap.min.css">
-    <title>Document</title>
+    <title>Bibliothèque</title>
 </head>
 <body>
 
